4098fd26d46c5c1677ef4d220465f61ae29241a8
Evan Drake <evanmdrake@icloud.com>
2023-11-17 16:24:35 -0800

    draft 1 version 1
    Signed-off-by: Evan Drake <evanmdrake@icloud.com>

--- code/fwp/src/Lib.hs
+++ code/fwp/src/Lib.hs
@@ -2,12 +2,16 @@ module Lib
     ( someFunc,
     setBoard,
     setWeights,
-    mapBoard,
+    mapMat,
     Player (White, Red),
     Board (Piece, Empty)
     ) where
 
--- TODO make mapBoard more general as a map over matrix board that returns a 'matrix a'
+{-TODO:
+  - rewrite setBoard to use mapMat to setup the initial Board
+  - create map for mvmt of peices based on current weights
+  - edit distance | levenstien distance
+ -}
 
 import Data.Matrix
 
@@ -34,20 +38,19 @@ setBoard =
     eventile = oscilateRow 0 8 True []
     oddtile = oscilateRow 1 8 True []
 
-mapBoardAux :: Int -> Int -> Matrix Board -> (Int -> Int -> Matrix Board -> Weights) -> [Weights] -> [[Weights]] -> Matrix Weights
-mapBoardAux r c mb f l wm
-  -- r+1 and c+1 b/c when indexing a matrix with getElem its one indexed :O
-  | r < dem && c < dem  = mapBoardAux r (c+1) mb f (f (r+1) (c+1) mb:l) wm
-  | r < dem && c >= dem = mapBoardAux (r+1) 0 mb f [] (l:wm)
+mapMatAux :: Int -> Int -> Matrix a -> (Int -> Int -> Matrix a -> b) -> [b] -> [[b]] -> Matrix b
+mapMatAux r c mb f l wm
+  | r < dem && c < dem  = mapMatAux r (c+1) mb f (f (r+1) (c+1) mb:l) wm
+  | r < dem && c >= dem = mapMatAux (r+1) 0 mb f [] (l:wm)
   | otherwise = fromLists wm where
   dem = nrows mb
 
--- | 'mapBoard' is a map transform over any Matrix 'Board'
--- where the function exposes (row -> column -> matrix 'Board' -> 'Weights')
--- 'mapBoard' will only take square matrices
-mapBoard :: Matrix Board -> (Int -> Int -> Matrix Board -> Weights) -> Matrix Weights
-mapBoard mb f = mapBoardAux 0 0 mb f [] []
+-- | 'mapMat is a map transform over any 'Matrix' a that exposes the row,column # to f
+-- where the function exposes (row -> column -> 'Matrix' a -> b)
+-- 'mapMat' will currently only take square matrices
+mapMat :: Matrix a -> (Int -> Int -> Matrix a -> b) -> Matrix b
+mapMat mb f = mapMatAux 0 0 mb f [] []
 
 -- | 'setWeights' analyses the state of the current Board and adjusts the weights accordingly
 setWeights :: Matrix Board -> Matrix Weights
-setWeights mb_t = mapBoard mb_t (\r c mb -> if mb!(r,c) == Piece Red || mb!(r,c) == Piece White then (0.0,0.0,1.0,0.0) else (0.0,0.0,0.0,0.0))
+setWeights mb_t = mapMat mb_t (\r c mb -> if mb!(r,c) == Piece Red || mb!(r,c) == Piece White then (0.0,0.0,1.0,0.0) else (0.0,0.0,0.0,0.0))
